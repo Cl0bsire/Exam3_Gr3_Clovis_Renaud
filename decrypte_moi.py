c6aeffc936c8637205e8c79d9068ddcf5f68c076
Cl0bsire <2584002@cegepoutaouais.qc.ca>
2025-11-18 17:51:34 -0500

    Commit-Push 3

--- decrypte_moi.py
+++ decrypte_moi.py
@@ -3,3 +3,22 @@ messages_gr3 = {
     "messages" : ["Rendez vous au point 8 à midi", "Plan B activer en cas de problème", "Le code maître est 2345"],
     "cryptes" : ["Uhqghc yrxv dx srlqw 1 à plgl", "Sodq E dfwlyhu hq fdv gh sureoèph", "Oh frgh pdîwuh hvw 4567"]
 }
+
+messages_decrypte=[]
+liste_alphabet=['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+liste_nombres=['1','2','3','4','5','6','7','8','9']
+
+for message in messages_gr3["cryptes"]:
+    decrypte = ""
+    for caractere in message:
+        if caractere in liste_alphabet:
+            decrypte += liste_alphabet[liste_alphabet.index(caractere)-4]
+        elif caractere in liste_nombres:
+            decrypte += liste_nombres[liste_nombres.index(caractere) - 4]
+        else:
+            decrypte+=caractere
+    messages_decrypte.append(decrypte)
+
+for i in range(len(messages_decrypte)):
+    if messages_decrypte[i]!=messages_gr3["messages"][i]:
+        print(f"Attention, le message {i+1} a été intercepté")
